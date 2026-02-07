13090c60c012b055741aa89a2deb97cf8b9b3b9f
Kaio Silveira <kaionpsa@gmail.com>
2026-02-07 20:16:19 -0300

    feat: creating controllers

---
+++ src/main/java/com/kaio/picpay_simplificado/controllers/TransactionController.java
@@ -0,0 +1,26 @@
+package com.kaio.picpay_simplificado.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kaio.picpay_simplificado.dtos.TransactionDTO;
+import com.kaio.picpay_simplificado.services.TransactionService;
+
+@RestController // API route
+@RequestMapping("/transactions") // base address localhost:8080/transactions
+public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping // accept POST request
+    public ResponseEntity<Void> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        this.transactionService.createTransaction(transaction);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
